40e48677d53abd87598d7d658d526ae6a3504bf2
deaxtivate <102901885+deaxtivate@users.noreply.github.com>
2022-12-20 05:16:48 +0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,5 +2,5 @@ const button = document.getElementById('fancy-button');
 
 button.addEventListener('click', () => {
   /* Do something when the button is clicked */
-  alert('You clicked the fancy button!');
+  alert('Tớ thích cậu <3');
 });
