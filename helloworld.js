1fd6e58b1512fc9e94cf38deacfd5a8590a8fde3
deaxtivate <102901885+deaxtivate@users.noreply.github.com>
2022-12-20 05:11:42 +0700

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('fancy-button');
+
+button.addEventListener('click', () => {
+  /* Do something when the button is clicked */
+  alert('You clicked the fancy button!');
+});
