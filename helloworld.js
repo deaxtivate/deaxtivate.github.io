d76d59e95dee2de8274f5fdcf015d38668c43976
deaxtivate <102901885+deaxtivate@users.noreply.github.com>
2022-12-20 09:40:46 +0700

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('fancy-button');
+function myFacebook(){
+  window.location= 'tym.html';
+}
+
+button.addEventListener('click', () => {
+  /* Do something when the button is clicked */
+  myFacebook();
+});
