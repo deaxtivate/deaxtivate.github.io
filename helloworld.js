a717608445870f12eef3fbf0d3ee410aecd8a311
deaxtivate <102901885+deaxtivate@users.noreply.github.com>
2022-12-20 09:40:14 +0700

    Delete helloworld.js

--- helloworld.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('fancy-button');
-
-button.addEventListener('click', () => {
-  /* Do something when the button is clicked */
-  alert('Tớ thích cậu <3');
-});
